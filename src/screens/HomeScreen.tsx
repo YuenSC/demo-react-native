3e424564b4ca1209328e4eb83bbb05f68d0c5d8c
Calvin Yuen <calvin.yuen@app-bar.com>
2023-11-25 19:01:13 +0800

    feat: Header Animation (control when onEndDrag)

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 import { Appbar, Text } from "react-native-paper";
 import Animated, {
   interpolate,
@@ -7,51 +7,51 @@ import Animated, {
   useSharedValue,
   withTiming,
 } from "react-native-reanimated";
-import {
-  SafeAreaView,
-  useSafeAreaInsets,
-} from "react-native-safe-area-context";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HeaderHeight = 64;
 
+const clamp = (value: number, lowerBound: number, upperBound: number) => {
+  "worklet";
+  return Math.min(Math.max(lowerBound, value), upperBound);
+};
+
 const HomeScreen = () => {
-  const translationY = useSharedValue(0);
-  const hideHeader = useSharedValue(false);
   const insets = useSafeAreaInsets();
+  const maxHeaderHeight = HeaderHeight + insets.top;
+  const minHeaderHeight = insets.top;
+  const headerHeight = useSharedValue(maxHeaderHeight);
 
   const scrollHandler = useAnimatedScrollHandler<{ startY: number }>({
-    onScroll: (event, ctx) => {
-      const diff = event.contentOffset.y - ctx.startY;
-      hideHeader.value = diff > 0 ? diff > 10 : diff < -10;
-      translationY.value = event.contentOffset.y;
-    },
     onBeginDrag: (event, ctx) => {
+      console.log("onBeginDrag");
       ctx.startY = event.contentOffset.y;
+    },
+    onEndDrag: (event, ctx) => {
+      console.log("onEndDrag");
+      const diff = event.contentOffset.y - ctx.startY;
+      headerHeight.value = diff > 0 ? minHeaderHeight : maxHeaderHeight;
     },
   });
 
   const headerStyle = useAnimatedStyle(() => {
     return {
       backgroundColor: "black",
-      opacity: withTiming(hideHeader.value ? 0 : 1, { duration: 500 }),
-    };
-  });
-
-  const testStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          translateY: withTiming(hideHeader.value ? -HeaderHeight : 0, {
-            duration: 500,
-          }),
-        },
-      ],
+      opacity: withTiming(
+        interpolate(
+          headerHeight.value,
+          [minHeaderHeight, maxHeaderHeight],
+          [0, 1]
+        ),
+        { duration: 300 }
+      ),
+      height: withTiming(headerHeight.value, { duration: 300 }),
     };
   });
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[headerStyle, testStyle]}>
+      <Animated.View style={headerStyle}>
         <Appbar.Header mode="small" style={styles.appBarHeader}>
           <Appbar.Content title="Calvin" titleStyle={{ color: "white" }} />
           <Appbar.Action icon="calendar" onPress={() => {}} />
@@ -59,68 +59,14 @@ const HomeScreen = () => {
         </Appbar.Header>
       </Animated.View>
 
-      <Animated.ScrollView
-        scrollEventThrottle={16}
-        onScroll={scrollHandler}
-        style={testStyle}
-      >
-        <Text style={styles.whiteColor}>HomeScreen</Text>
-        <Text variant="bodyLarge" style={styles.whiteColor}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-          veritatis accusamus explicabo sint incidunt non perspiciatis
-          assumenda! Neque aut earum repudiandae similique dicta sint quod qui
-          magni! Dignissimos, quas vel earum atque ratione eos inventore et
-          voluptates, dolorem voluptas, ullam at odit eveniet numquam! Atque
-          delectus cum debitis earum eos dolores odit fugit sequi temporibus
-          iure. Mollitia minus, cum, fugit nulla dolorum tempore, pariatur
-          veniam obcaecati ipsa enim quisquam reiciendis aperiam reprehenderit
-          impedit magnam odio rerum consectetur necessitatibus asperiores nobis
-          assumenda adipisci dignissimos repellat. Voluptatem veniam hic aliquam
-          ducimus atque esse cum, ab maxime minima ut architecto assumenda
-          voluptatum doloremque.
-        </Text>
-        <Text variant="bodyLarge" style={styles.whiteColor}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-          veritatis accusamus explicabo sint incidunt non perspiciatis
-          assumenda! Neque aut earum repudiandae similique dicta sint quod qui
-          magni! Dignissimos, quas vel earum atque ratione eos inventore et
-          voluptates, dolorem voluptas, ullam at odit eveniet numquam! Atque
-          delectus cum debitis earum eos dolores odit fugit sequi temporibus
-          iure. Mollitia minus, cum, fugit nulla dolorum tempore, pariatur
-          veniam obcaecati ipsa enim quisquam reiciendis aperiam reprehenderit
-          impedit magnam odio rerum consectetur necessitatibus asperiores nobis
-          assumenda adipisci dignissimos repellat. Voluptatem veniam hic aliquam
-          ducimus atque esse cum, ab maxime minima ut architecto assumenda
-          voluptatum doloremque.
-        </Text>
-        <Text variant="bodyLarge" style={styles.whiteColor}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-          veritatis accusamus explicabo sint incidunt non perspiciatis
-          assumenda! Neque aut earum repudiandae similique dicta sint quod qui
-          magni! Dignissimos, quas vel earum atque ratione eos inventore et
-          voluptates, dolorem voluptas, ullam at odit eveniet numquam! Atque
-          delectus cum debitis earum eos dolores odit fugit sequi temporibus
-          iure. Mollitia minus, cum, fugit nulla dolorum tempore, pariatur
-          veniam obcaecati ipsa enim quisquam reiciendis aperiam reprehenderit
-          impedit magnam odio rerum consectetur necessitatibus asperiores nobis
-          assumenda adipisci dignissimos repellat. Voluptatem veniam hic aliquam
-          ducimus atque esse cum, ab maxime minima ut architecto assumenda
-          voluptatum doloremque.
-        </Text>
-        <Text variant="bodyLarge" style={styles.whiteColor}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-          veritatis accusamus explicabo sint incidunt non perspiciatis
-          assumenda! Neque aut earum repudiandae similique dicta sint quod qui
-          magni! Dignissimos, quas vel earum atque ratione eos inventore et
-          voluptates, dolorem voluptas, ullam at odit eveniet numquam! Atque
-          delectus cum debitis earum eos dolores odit fugit sequi temporibus
-          iure. Mollitia minus, cum, fugit nulla dolorum tempore, pariatur
-          veniam obcaecati ipsa enim quisquam reiciendis aperiam reprehenderit
-          impedit magnam odio rerum consectetur necessitatibus asperiores nobis
-          assumenda adipisci dignissimos repellat. Voluptatem veniam hic aliquam
-          ducimus atque esse cum, ab maxime minima ut architecto assumenda
-          voluptatum doloremque.
-        </Text>
+      <Animated.ScrollView scrollEventThrottle={16} onScroll={scrollHandler}>
+        {Array.from(Array(10).keys()).map((index) => (
+          <Image
+            source={{ uri: "https://picsum.photos/400/300?" + index }}
+            style={styles.image}
+            resizeMode="contain"
+          />
+        ))}
       </Animated.ScrollView>
     </View>
   );
@@ -139,6 +85,11 @@ const styles = StyleSheet.create({
   appBarHeader: {
     backgroundColor: "black",
     borderBottomWidth: 1,
-    borderColor: "white",
+    borderColor: "lightgray",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 4 / 3,
+    marginBottom: 16,
   },
 });
