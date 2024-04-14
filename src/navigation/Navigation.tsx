db40b2dbb5b0b88cd0f7d9ccb132873c01335de8
Calvin Yuen <scyuenab@gmail.com>
2024-04-15 02:16:55 +0800

    feat: Add analytics

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,13 +1,43 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { memo } from "react";
+import analytics from "@react-native-firebase/analytics";
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from "@react-navigation/native";
+import { memo, useRef } from "react";
 
 import StackNavigator from "./navigators/StackNavigator";
 
-const Navigation = memo(() => (
-  <NavigationContainer>
-    <StackNavigator />
-  </NavigationContainer>
-));
+import { IStackParamList } from "@/types/navigation";
+
+const Navigation = memo(() => {
+  const routeNameRef = useRef<string | null>(null);
+  const navigationRef = useRef<NavigationContainerRef<IStackParamList>>(null);
+
+  return (
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        routeNameRef.current =
+          navigationRef.current?.getCurrentRoute()?.name || null;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current?.getCurrentRoute()?.name;
+
+        if (!currentRouteName) return;
+        if (previousRouteName !== currentRouteName) {
+          await analytics().logScreenView({
+            screen_name: currentRouteName,
+            screen_class: currentRouteName,
+          });
+        }
+        routeNameRef.current = currentRouteName;
+      }}
+    >
+      <StackNavigator />
+    </NavigationContainer>
+  );
+});
 
 Navigation.displayName = "Navigation";
 
