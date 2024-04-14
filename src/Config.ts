cbb9a021ff94f83ee0c22f59aa7acfc4df050fa2
Calvin Yuen <scyuenab@gmail.com>
2024-04-14 23:05:20 +0800

    fix: Update ad unit id

--- src/Config.ts
+++ src/Config.ts
@@ -3,20 +3,22 @@ import { TestIds } from "react-native-google-mobile-ads";
 
 import { version } from "../package.json";
 
-const adUnitId =
-  Platform.select({
-    ios: "ca-app-pub-3349863138501385~9578743941",
-    android: "",
-  }) || "";
-
 const Config = {
   apiUrl: process.env.EXPO_PUBLIC_API_URL,
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
   isDev: __DEV__,
   version,
-  adBannerUnitId: __DEV__ ? TestIds.ADAPTIVE_BANNER : adUnitId,
-  adAppOpenUnitId: __DEV__ ? TestIds.APP_OPEN : adUnitId,
-  adInterstitialUnitId: __DEV__ ? TestIds.INTERSTITIAL : adUnitId,
+  adBannerUnitId: __DEV__
+    ? TestIds.ADAPTIVE_BANNER
+    : Platform.select({
+        ios: "ca-app-pub-3349863138501385/2202800899",
+        android: "",
+      }) || "",
+  adInterstitialUnitId: __DEV__
+    ? TestIds.INTERSTITIAL
+    : Platform.select({
+        ios: "ca-app-pub-3349863138501385/4558018413",
+      }) || "",
 };
 
 export default Config;
